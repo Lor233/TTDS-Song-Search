0860198f6d65ecb1217e3f529e7aad1637b26206
Lor233 <endtolor@gmail.com>
2022-02-26 20:29:56 +0000

    update simple search

--- songsearch/search.py
+++ songsearch/search.py
@@ -27,16 +27,16 @@ def invertedIndex():
         lyrics = np.array([x for x in lyrics if x])
         for sen_pos, lyric in enumerate(lyrics):
             for word_pos, token in enumerate(stem(lyric)):
-                token_db = list(db.inverted_index.find({ 'token': token }))
+                # token_db = list(db.inverted_index.find({ 'token': token, 'song_id': song['_id']}))
                 db.words.insert_one(
                     { 'token': token, 'song_id': song['_id'], 'sen_pos': sen_pos, 'word_pos': word_pos }
                 )
-                if len(token_db) == 0:
-                    db.inverted_index.insert_one(
-                        { 'token': token, 'song_id': song['_id'], 'count': 1 }
-                    )
-                else:
-                    db.inverted_index.update_one(
-                        { '_id': token_db[0]['_id'], 'song_id': song['_id'] }, 
-                        { '$set': { 'count': token_db[0]['count'] + 1 } }
-                    )
+                # if len(token_db) == 0:
+                #     db.inverted_index.insert_one(
+                #         { 'token': token, 'song_id': song['_id'], 'count': 1 }
+                #     )
+                # else:
+                #     db.inverted_index.update_one(
+                #         { '_id': token_db[0]['_id'], 'song_id': song['_id'] }, 
+                #         { '$set': { 'count': token_db[0]['count'] + 1 } }
+                #     )
